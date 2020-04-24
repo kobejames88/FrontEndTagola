81f163b8cc7b018d6b2fec4ee7c32df608b2eb5f
michaelou <1934615110@qq.comomm>
2020-04-24 18:26:04 +0800

    fix

---
+++ src/layout/index.js
@@ -0,0 +1,18 @@
+import bread from './bread.vue'
+import headNav from './headNav.vue'
+import leftMenu from './leftMenu.vue'
+import Layout from './home.vue'
+import Content from './content.vue'
+import footerNav from './footerNav.vue'
+import topMenu from './topMenu.vue'
+import Vue from 'vue'
+
+export {
+    Layout,
+    Content,
+    bread,
+    headNav,
+    leftMenu,
+    footerNav,
+    topMenu
+}
